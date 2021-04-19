08be8f2c0e54d59bc47581d65885cf4d9dd33518
Corentin Cadiou <corentin.cadiou@cphyc.me>
2021-04-19 20:39:53 +0100

    Adding more tests

--- tests/test_rst.py
+++ tests/test_rst.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
 from textwrap import dedent
 
-from pre_commit_all_the_way_down.python_doc import apply_pre_commit_rst
+from pre_commit_all_the_way_down.python_doc import (
+    apply_pre_commit_on_file,
+    apply_pre_commit_rst,
+)
 
 
 def test_format_from_string():
@@ -34,3 +39,53 @@ def test_format_from_string():
 
     out = apply_pre_commit_rst(before, whitelist=["black", "isort", "flake8"])
     assert out == (1, after)
+
+
+def test_format_from_file():
+    before = dedent(
+        """\
+        .. python-script::
+
+            import toto
+            import numpy as np
+            np.random.rand(10,10,10)
+            msg = 'yeeeeeet';
+        """
+    )
+
+    after = dedent(
+        """\
+        .. python-script::
+
+            import numpy as np
+            import toto
+
+            np.random.rand(10, 10, 10)
+            msg = "yeeeeeet"
+        """
+    )
+
+    with TemporaryDirectory() as dirname:
+        f = Path(dirname) / "test.rst"
+
+        f.write_text(before)
+        retcode = apply_pre_commit_on_file(str(f), skiplist=["flake8"])
+        modified = f.read_text()
+        assert (retcode, modified) == (1, after)
+
+        f.write_text(before)
+        retcode = apply_pre_commit_on_file(
+            str(f), whitelist=["black", "isort", "flake8"]
+        )
+        modified = f.read_text()
+        assert (retcode, modified) == (1, after)
+
+        f.write_text(before)
+        retcode = apply_pre_commit_on_file(str(f), whitelist=["black", "isort"])
+        modified = f.read_text()
+        assert (retcode, modified) == (1, after)
+
+        # NOTE: not updating the content this time, it should be unchanged now!
+        retcode = apply_pre_commit_on_file(str(f), whitelist=["black", "isort"])
+        modified = f.read_text()
+        assert (retcode, modified) == (0, after)
